4eee6c739ba22bca1a80c9b49f7561e6774f296b
Harsh <harshmehra53@gmail.com>
2023-05-22 22:41:09 +0530

    up

---
+++ Array.java
@@ -0,0 +1,21 @@
+// Creating an array
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        // String[] name;      // Declaring an array
+        // name = new String[5]; // Instantiation of an array
+        // name[0] = "Harsh"; // Initialization of an array 
+        // name[1] = "Juhi";
+        // name[2] = "Vasu";
+        // name[3] = "raji";
+        // name[4] = "Shivam";
+
+        // System.out.println(Arrays.toString(name));
+
+        String friends[] = {"Harsh", "Juhi", "Vasu"}; // Instead of using upper method to create an array we can use thois method too. 
+
+        System.out.println(Arrays.toString(friends));
+    }
+}
